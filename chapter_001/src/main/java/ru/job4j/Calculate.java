c85c4954e367e537d16179447b7ce4aef2db4164
Mikhail Kubar <m.kubar@mail.ru>
2017-08-25 15:30:30 +0300

    Настройка IDEA[#22902]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -6,19 +6,19 @@ package ru.job4j;
  *@since 25.08.2017.
  */
 public class Calculate {
-/**
- * Main.
- *@param args is args
- */
- public static void main(String[] args) {
-       		System.out.println("Hello world.");
-    	}
-/**
- * Method echo.
- * @param name Your name.
- * @return Echo plus your name.
- */
-	public String echo(String name) {
+	/**
+	 * Main.
+	 *@param args is args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world.");
+	}
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}
 }
