8a35fccaa7ea8a3698bd0df75710ca2f29f8bf26
Mikhail Kubar <m.kubar@mail.ru>
2017-08-25 13:00:52 +0300

    JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -4,9 +4,22 @@ package ru.job4j;
  *Class Calculate Решение задачи. Часть 001, урок1.
  *@author Mikhail Kubar
  *@since 25.08.2017
-*/
+ */
 public class Calculate {
+/**
+ * Main
+ *@param args
+ */ 
 	public static void main(String[] args) {
        		System.out.println("Hello world.");
     	}
+		
+/**
+ * Method echo.
+ * @param name Your name.
+ * @return Echo plus your name.
+ */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
 }
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+/**
+ * 
+ * 
+ * These classes contain the test and 
+ * metods of the produce "Calculate"
+ * 
+ * @since 25.08.17
+ * @author Mikhail Kubar
+ * @version 1.0
+ */
