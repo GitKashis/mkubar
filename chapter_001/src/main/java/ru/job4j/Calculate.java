c955af917ba8047b05913e1c41b3546eb24b2223
Mikhail Kubar <m.kubar@mail.ru>
2017-08-25 12:18:13 +0300

    JavaDoc. task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,11 @@
 package ru.job4j;
 
+/**
+ *Class Calculate Решение задачи. Часть 001, урок1.
+ *@author Mikhail Kubar
+ *@since 25.08.2017
+*/
 public class Calculate {
-
 	public static void main(String[] args) {
        		System.out.println("Hello world.");
     	}
