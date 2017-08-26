52e4cec2b669184b1b82d40c473c4fcc089657a1
Mikhail Kubar <m.kubar@mail.ru>
2017-08-26 23:37:41 +0400

    Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,17 @@
+package ru.job4j.max;
+
+/**
+ * Class Max. Решение задачи. Часть 001, урок 3.1
+ * @author Mikhail Kubar
+ * @since 26.08.2017
+ */
+public class Max {
+    /**
+     * Определение максимума.
+     * @param first - первое число.
+     * @param second - второе.
+     */
+    public int max(int first, int second) {
+        return (first > second ? first : second);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Mikhail Kubar.
+ * @version 1.0.
+ * @since 26.08.2017.
+ */
+public class MaxTest {
+    /**
+     * Test Max.
+     * Метод max из класса Max.
+     */
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+}
