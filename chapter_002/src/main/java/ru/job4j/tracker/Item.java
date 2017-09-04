093f147a70d5f957e66d66387290647be783e8e2
Mikhail Kubar <m.kubar@mail.ru>
2017-09-05 00:58:07 +0300

    Tracker testing...

--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -1,22 +1,42 @@
 package ru.job4j.tracker;
 
+/**
+ * Класс описывает сущность "Заявка", является элементом массива items[].
+ *
+ * @author Mikhail Kubar
+ * @version 1.0
+ * @since 04.09.17
+ */
 public class Item {
-
+    // уникальный id, служит для идентивикации записи в массиве.
     private String Id;
+    // имя заявки.
     public String name;
+    // описание.
     public String description;
+    // время создания.
     public long create;
+    // коментарий.
+    public String comment;
 
 
-
-    public Comment comment;
+    /**
+     * дефолтный конструктор.
+     */
     public Item(){
     }
 
-    public Item(String name, String description, long create){
+    /**
+     * конструктор с параметрами.
+     * @param name name
+     * @param description desc
+     * @param create create
+     */
+    public Item(String name, String description, long create, String comment){
         this.name = name;
         this.description = description;
         this.create = create;
+        this.comment = comment;
     }
 
     public String getName() {
@@ -39,10 +59,11 @@ public class Item {
         this.Id = id;
     }
 
-    public void setComment(Comment comment) {
+    public void setComment(String comment) {
         this.comment = comment;
     }
-    public Comment getComment() {
+
+    public String getComment() {
         return this.comment;
     }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -2,54 +2,138 @@ package ru.job4j.tracker;
 
 import java.util.Random;
 
+/**
+ * Метод хранит массив заявок items, реализует операции CRUD для объектов типа Item.
+ *
+ * @author Mikhail Kubar
+ * @version 1.0
+ * @since 04.09.17
+ */
 public class Tracker {
-
-    private Item[] items = new Item[10];
+    //выделяем память для массива заявок Item на 100 элементов.
+    private Item[] items = new Item[100];
+    //индекс массива Items[].
     private int position = 0;
+    //случайное число для присвоения Id.
     private static final Random rn = new Random();
 
+    /**
+     * Метод присваивает новой заявке рандомный Id и добавляет в конец массива items.
+     *
+     * @param item - новая заявка.
+     * @return Item item.
+     */
     public Item add(Item item) {
         item.setId(this.generateId());
         this.items[position++] = item;
         return item;
     }
-    public Item add(Comment comment) {
 
-        Item result = this.items[1].setComment(comment);
-
-        return result;
+    /**
+     * Метод добавляет комментарий к указанному элементу массива.
+     *
+     * @param commentItem - заявка, для которой нужно добавить коментарий.
+     * @param comment     - коментарий.
+     * @return item.
+     */
+    public Item add(Item commentItem, String comment) {
+        //находим в массиве заявку, для которой надо добавить коментарий.
+        Item item = findById(commentItem.getId());
+        if (item != null) {
+            item.setComment(comment);
+        }
+        return item;
     }
 
-    protected Item findById (String id){
+    /**
+     * Метод перезаписывает элемент с соответствующим Id
+     *
+     * @param newItem - измененный элемент.
+     */
+    public void update(Item newItem) {
+//        // получаем ссылку на искомый элемент массива,
+//        // перекидываем эту ссылку на другой объект.
+//        Item item = findById(newItem.getId());
+//        item = newItem;
+
+        for (int i = 0; i < this.position; i++) {
+            if (this.items[i].getId().equals(newItem.getId())) {
+                this.items[i] = newItem;
+            }
+            break;
+        }
+    }
+
+    /**
+     * Метод создает новый массив на 1 элемент меньше,
+     * находит элемент с соответствующим Id,
+     * копирует в новый массив все элементы кроме найденного с индексом i.
+     *
+     * @param delItem - удаляемый элемент.
+     */
+    public void delete(Item delItem) {
+        int i;
+        Item[] result = new Item[this.items.length - 1];
+        for (i = 0; i < this.items.length; i++) {
+            if (this.items[i].getId().equals(delItem.getId()))
+                break;
+        }
+        // индекс найден. Копируем левую часть.
+        System.arraycopy(items, 0, result, 0, i - 1);
+        //копируем оставшуюся правую часть.
+        System.arraycopy(items, i + 1, result, i, items.length - i - 1);
+        //именьшаем текущую позицию в массиве
+        position--;
+    }
+
+    /**
+     * Метод ищет в массиве запись с соответствующим Id.
+     *
+     * @param id уникальный ключ для элементов массива.
+     * @return item - элемент массива.
+     */
+    protected Item findById(String id) {
         Item result = null;
-        for (Item item: items) {
-            if(item != null && item.getId().equals(id))
+        for (Item item : items) {
+            if (item != null && item.getId().equals(id))
                 result = item;
             break;
         }
         return result;
     }
-    protected Item findByName (String id){
+
+    /**
+     * Метод ищет элемент по заданному имени.
+     * @param name - имя в заявке
+     * @return item - элемент массива.
+     */
+    protected Item findByName(String name) {
         Item result = null;
-        for (Item item: items) {
-            if(item != null && item.getName().equals(id))
+
+        for (Item item : items) {
+            if (item != null && item.getName().equals(name)) {
                 result = item;
+            }
             break;
         }
         return result;
     }
 
 
-    String generateId() {
-        return String.valueOf(rn.nextInt());
-    }
-
-    public  Item[] getAll() {
+    /**
+     * Метод возвращает копию массива this.items без null элементов.
+     * @return Item[]
+     */
+    public Item[] getAll() {
         Item result[] = new Item[position];
-        for (int i = 0; i != this.position; i++)
-              {
-                result[i] = this.items[i];
+        for (int i = 0; i != this.position; i++) {
+            result[i] = this.items[i];
         }
         return result;
+    }
+
+    // вспомогательный метод для генерации уникального ключа Id.
+    private String generateId() {
+        return String.valueOf(rn.nextInt());
     }
 }
