acb565d1e4b78811bae7035d6d4f90b11f55f665
Mikhail Kubar <m.kubar@mail.ru>
2017-11-13 23:30:41 +0300

    Тестовое задание на сортировку

---
+++ chapter_002/src/main/java/ru/job4j/sorting/ExpandSet.java
@@ -0,0 +1,48 @@
+package ru.job4j.sorting;
+
+import java.util.*;
+
+public class ExpandSet {
+
+
+    /*
+  Классы-компараторы.
+ */
+    class Comp implements Comparator<List<String>> {
+
+        @Override
+        public int compare(List<String> o1, List<String> o2) {
+            if (o1.size() < o2.size()) return -1;
+                else if (o1.size() == o2.size()) return 0;
+                    else return 1;
+        }
+    }
+
+
+    /**
+     * Метод разбивает входящий список на подгруппы .
+     * @param  source список чисел на вход.
+     * @return на выходе список из расширяющихся последовательно списков
+     */
+    public Set<String[]> expand(String[] source) {
+
+        // множество массивов
+        Set<String[]> result = new HashSet<>();
+
+        for (String aString : source) {
+
+            String[] deps = aString.split("/");
+
+            List<String> sum = new ArrayList();
+            String[] subStr;
+
+            for (String values : deps) {
+
+                sum.add(values);
+                subStr = sum.toArray(new String[sum.size()]);
+                result.add(subStr);
+            }
+        }
+        return result;
+    }
+}
--- chapter_002/src/test/java/ru/job4j/sorting/ExpandTest.java
+++ chapter_002/src/test/java/ru/job4j/sorting/ExpandTest.java
@@ -4,18 +4,34 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ExpandTest {
+
+    String[] source = new String[]{
+            "K1/SK1",
+            "K1/SK2",
+            "K1/SK1/SSK1",
+            "K1/SK1/SSK2",
+            "K2",
+            "K2/SK1/SSK1",
+            "K2/SK1/SSK2"
+    };
+
+
     @Test
     public void expand() throws Exception {
-    Expand expand = new Expand();
-        List<Integer> value = Arrays.asList(1, 2, 3);
-        List<List<Integer>> result = expand.expand(value);
 
-        System.out.println(result.toString());
-        //assertThat(result, is(expected));
+    ExpandSet expand = new ExpandSet();
+
+        Set<String[]> result = expand.expand(source);
+        System.out.println("Проверка добавления элементов:");
+        result.forEach(o -> System.out.println(Arrays.toString(o)));
+
+        assertThat(result.size(), is(9));
     }
 
 }
