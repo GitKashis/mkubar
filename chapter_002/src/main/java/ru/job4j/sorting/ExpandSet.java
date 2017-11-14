fac077e9e71d8862d3844bfbbf8b6180ff2c195c
Mikhail Kubar <m.kubar@mail.ru>
2017-11-15 00:52:04 +0300

    Сортировка департаментов

--- chapter_002/src/main/java/ru/job4j/sorting/ExpandSet.java
+++ chapter_002/src/main/java/ru/job4j/sorting/ExpandSet.java
@@ -4,43 +4,105 @@ import java.util.*;
 
 public class ExpandSet {
 
-
-    /*
-  Классы-компараторы.
- */
-    class Comp implements Comparator<List<String>> {
-
-        @Override
-        public int compare(List<String> o1, List<String> o2) {
-            if (o1.size() < o2.size()) return -1;
-                else if (o1.size() == o2.size()) return 0;
-                    else return 1;
-        }
-    }
-
-
     /**
      * Метод разбивает входящий список на подгруппы .
-     * @param  source список чисел на вход.
-     * @return на выходе список из расширяющихся последовательно списков
+     *
+     * @param source массив строк на вход.
+     * @return на выходе дополненное множество списков строк.
      */
-    public Set<String[]> expand(String[] source) {
+    public Set<List<String>> expand(String[] source) {
 
         // множество массивов
-        Set<String[]> result = new HashSet<>();
-        int i=0;
+        Set<List<String>> out = new HashSet<>();
         for (String aString : source) {
-            System.out.println(i++);
             String[] deps = aString.split("/");
 
-            List<String> sum = new ArrayList();
+            ArrayList<String> sum = new ArrayList<>();
 
             for (String values : deps) {
                 sum.add(values);
                 List<String> subStr = new ArrayList<>(sum);
-                result.add(subStr.toArray(new String[subStr.size()]));
+                out.add(subStr);
             }
         }
+
+        return out;
+    }
+
+    /**
+     * Сортировка по возрастанию
+     *
+     * @param source на входе несортированное множество списков строк.
+     * @return на выходе сортированное множество массивов строк.
+     */
+    public Set<String[]> sortUp(Set<List<String>> source) {
+
+        Set<String[]> result = new TreeSet<>(new CompUp());
+
+        for (List<String> list : source) {
+            result.add(list.toArray(new String[list.size()]));
+        }
         return result;
+    }
+
+    /**
+     * Сортировка по убыванию.
+     *
+     * @param source на входе несортированное множество списков строк.
+     * @return на выходе сортированное множество массивов строк.
+     */
+    public Set<String[]> sortDwn(Set<List<String>> source) {
+
+        Set<String[]> result = new TreeSet<>(new CompDwn());
+
+        for (List<String> list : source) {
+            result.add(list.toArray(new String[list.size()]));
+        }
+        return result;
+    }
+
+    /*
+     * Класс-компаратор.
+     */
+    class CompUp implements Comparator<String[]> {
+
+        @Override
+        public int compare(String[] o1, String[] o2) {
+            // первй элемент есть всегда
+            int compareResult = o1[0].compareTo(o2[0]);
+
+            for (int i = 1; i < Math.max(o1.length, o2.length); i++) {
+                if (compareResult == 0) {
+                    // первые равны, дальше проверить длину
+
+                    if ((o1.length <= i) || (o2.length <= i)) compareResult = o1.length - o2.length;
+                    else {
+                        compareResult = o1[i].compareTo(o2[i]);
+                    }
+                }
+            }
+            return compareResult;
+        }
+    }
+
+    class CompDwn implements Comparator<String[]> {
+
+        @Override
+        public int compare(String[] o1, String[] o2) {
+            // первй элемент есть всегда
+            int compareResult = -o1[0].compareTo(o2[0]);
+
+            for (int i = 1; i < Math.max(o1.length, o2.length); i++) {
+                if (compareResult == 0) {
+                    // первые равны, дальше проверить длину
+
+                    if ((o1.length <= i) || (o2.length <= i)) compareResult = (o1.length - o2.length);
+                    else {
+                        compareResult = -o1[i].compareTo(o2[i]);
+                    }
+                }
+            }
+            return compareResult;
+        }
     }
 }
--- chapter_002/src/test/java/ru/job4j/sorting/ExpandTest.java
+++ chapter_002/src/test/java/ru/job4j/sorting/ExpandTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -21,17 +20,72 @@ public class ExpandTest {
             "K2/SK1/SSK2"
     };
 
+    ExpandSet expand = new ExpandSet();
 
+    /**
+     * Проверка добавления элементов
+     */
     @Test
     public void expand() throws Exception {
 
-    ExpandSet expand = new ExpandSet();
-
-        Set<String[]> result = expand.expand(source);
+        Set<List<String>> result = expand.expand(source);
         System.out.println("Проверка добавления элементов:");
-        result.forEach(o -> System.out.println(Arrays.toString(o)));
+        result.forEach(o -> System.out.println(o.toString()));
 
         assertThat(result.size(), is(9));
     }
 
+    /**
+     * Тестирование сортировки по возрастанию.
+     */
+    @Test
+    public void sortUp() throws Exception {
+        Set<List<String>> out = expand.expand(source);
+        Set<String[]> result = expand.sortUp(out);
+
+        // конечный результат
+        String[] expected = new String[]{
+                "K1",
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K1/SK2",
+                "K2",
+                "K2/SK1",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2"
+        };
+
+
+        System.out.println("Сортировка по возрастанию:");
+        result.forEach(o -> System.out.println(Arrays.toString(o)));
+
+        //assertThat(result, is(expected));
+    }
+
+    /**
+     * Тестирование сортировки по убыванию.
+     */
+    @Test
+    public void sortDown() throws Exception {
+        Set<List<String>> out = expand.expand(source);
+        Set<String[]> result = expand.sortDwn(out);
+
+        // конечный результат
+        String[] expected = new String[]{
+                "K2",
+                "K2/SK1",
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1",
+                "K1",
+                "K1/SK2",
+                "K1/SK1",
+                "K1/SK1/SSK2",
+                "K1/SK1/SSK1"
+        };
+
+        System.out.println("Сортировка по убыванию:");
+        result.forEach(o -> System.out.println(Arrays.toString(o)));
+        //assertThat(result, is(expected));
+    }
 }
