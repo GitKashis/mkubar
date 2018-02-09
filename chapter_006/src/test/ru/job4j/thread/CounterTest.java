0c0c20f454adb136b2c810af58317bbd0fe66b36
Mikhil Kubar <m.kubar@mail.ru>
2018-02-09 18:05:33 +0300

    Fix thread join()

---
+++ chapter_006/src/test/ru/job4j/thread/CounterTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.thread;
+
+
+import org.junit.Test;
+
+/**
+ * Class for testing threads.
+ *
+ */
+public class CounterTest {
+    private String text = "The Java Virtual Machine allows an application to have " +
+            "multiple threads of execution running concurrently";
+    private Thread t1, t2;
+
+    @Test
+    public void analizeText() throws InterruptedException {
+        System.out.println("Start analizing text");
+        t1 = new Thread(new LettersCount(text), "t1");
+        t2 = new Thread(new SpacesCount(text), "t2");
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Finish analizing text");
+    }
+}
+
+        
+                
