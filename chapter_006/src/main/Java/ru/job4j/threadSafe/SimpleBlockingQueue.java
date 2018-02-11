a0b8691e362eaa12ceb04ecb53a994c6374e3ebc
Mikhail Kubar <m.kubar@mail.ru>
2018-02-11 22:47:28 +0300

    Реализация блокирующей очереди

---
+++ chapter_006/src/main/Java/ru/job4j/threadSafe/SimpleBlockingQueue.java
@@ -0,0 +1,70 @@
+package ru.job4j.threadSafe;
+
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Это блокирующая очередь, ограниченная по размеру.
+ * В данном шаблоне Producer помещает данные в очередь, а Consumer извлекает данные из очереди.
+ * Если очередь заполнена полностью, то при попытке добавления поток Producer блокируется до тех пор,
+ * пока Consumer не извлечет очередные данные, т.е. в очереди появится свободное место.
+ * И наоборот если очередь пуста поток Consumer блокируется, до тех пор пока Producer не поместит в очередь данные.
+ */
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+    private int limit;
+
+    public SimpleBlockingQueue(int limit) {
+        this.limit = limit;
+    }
+
+    /**
+     * Метод добавляет элемент в конец очереди.
+     *
+     * @param value - item.
+     * @return true / false.
+     */
+    public synchronized boolean offer(T value) throws InterruptedException {
+        System.out.println("[BlockingQueue]: try to put: " + value );
+        while (this.queue.size() == this.limit) {
+            System.out.println("[BlockingQueue]: queue is full, waiting");
+            this.wait();
+        }
+        if (this.queue.size() < this.limit) {
+            System.out.println("[BlockingQueue]: there was a place, notify");
+            this.notifyAll();
+        }
+
+        return this.queue.offer(value);
+    }
+
+    /**
+     * Извлечение элемента из начала  очереди.
+     * Если очередь пуста, поток становится вожидание.
+     * Если заполнена, то извлекаем элемент и говорим потокам, что есть свободное место.
+     *
+     * @return T item.
+     */
+    public synchronized T pool() throws InterruptedException {
+        System.out.println("[BlockingQueue]: try to take");
+        while (this.queue.size() == 0){
+            System.out.println("[BlockingQueue]: queue is empty, waiting until a put");
+            this.wait();
+        }
+
+        if (this.queue.size() != 0) {
+            System.out.println("[BlockingQueue]: there are items in the queue, notify");
+            this.notifyAll();
+        }
+
+        T item = this.queue.remove();
+        System.out.println("[BlockingQueue]: take ok: " + item );
+        return item;
+    }
+}
---
+++ chapter_006/src/test/ru/job4j/threadSafe/SimpleBlockingQueueTest.java
@@ -0,0 +1,134 @@
+package ru.job4j.threadSafe;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+public class SimpleBlockingQueueTest {
+
+    /**
+     * Класс-поставщик.
+     */
+    class Producer implements Runnable {
+        private final SimpleBlockingQueue<Integer> queue;
+
+        public Producer(SimpleBlockingQueue<Integer> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(String.format("[%s]: run", Thread.currentThread().getName()));
+            while (true) {
+                try {
+                    queue.offer(produce());
+                    Thread.currentThread().sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private Integer produce() {
+            Integer i = new Random().nextInt(100);
+            System.out.println(String.format("[%s]: produce %s", Thread.currentThread().getName(), i));
+            return i;
+        }
+    }
+
+    /**
+     * Класс-получатель.
+     */
+    class Consumer implements Runnable {
+        private final SimpleBlockingQueue<Integer> queue;
+
+        public Consumer(SimpleBlockingQueue<Integer> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(String.format("[%s]: run", Thread.currentThread().getName()));
+            while (true) {
+                try {
+                    int i = this.consume();
+                    System.out.println(String.format("[%s]: consume %s", Thread.currentThread().getName(), i));
+                    Thread.currentThread().sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private int consume() throws InterruptedException {
+            return queue.pool();
+        }
+    }
+
+    /**
+     * Тест 1 входящего и 1 исходящего потоков.
+     */
+    @Test
+    public void testTwoThread() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+
+        Thread producer = new Thread(new Producer(queue), "Producer");
+        Thread customer = new Thread(new Consumer(queue), "Consumer");
+
+        producer.start();
+        customer.start();
+
+        producer.join();
+        customer.join();
+    }
+
+    /**
+     * Если входящих потоков больше, то они быстрее заполняют очередь.
+     * Исходящий не успевает разбирать.
+     */
+    @Test
+    public void WhenThreeProducerOneCustomer() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+
+        Thread producer1 = new Thread(new Producer(queue), "Producer1");
+        Thread producer2 = new Thread(new Producer(queue), "Producer2");
+        Thread producer3 = new Thread(new Producer(queue), "Producer3");
+
+        Thread customer = new Thread(new Consumer(queue), "Customer");
+
+        producer1.start();
+        producer2.start();
+        producer3.start();
+        customer.start();
+
+        producer1.join();
+        producer2.join();
+        producer3.join();
+        customer.join();
+    }
+
+    /**
+     * Если входящих потоков больше, то они быстрее заполняют очередь.
+     * Исходящий не успевает разбирать.
+     */
+    @Test
+    public void WhenOneProducerThreeCustomer() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+
+        Thread producer = new Thread(new Producer(queue), "Producer");
+
+        Thread customer1 = new Thread(new Consumer(queue), "Customer1");
+        Thread customer2 = new Thread(new Consumer(queue), "Customer2");
+        Thread customer3 = new Thread(new Consumer(queue), "Customer3");
+
+        producer.start();
+        customer1.start();
+        customer2.start();
+        customer3.start();
+
+        producer.join();
+        customer1.join();
+        customer2.join();
+        customer3.join();
+    }
+}
