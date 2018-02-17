f73b975ee50deaa80852d17becab5ec4fb91bfee
Mikhail Kubar <m.kubar@mail.ru>
2018-02-17 17:33:29 +0300

    RTS_Lab1. SimpleThread Join.

--- chapter_006/src/main/Java/ru/job4j/thread/lab/SimpleThread.java
+++ chapter_006/src/main/Java/ru/job4j/thread/lab/SimpleThread.java
@@ -1,21 +1,35 @@
 package ru.job4j.thread.lab;
 
 /**
- *
+ * Класс, выполняющий вывод текстовой строки заданное количество раз
+ * и с установленным периодом.
  */
 public class SimpleThread implements Runnable {
-    private final  int count;
 
-    public SimpleThread(int count) {
-        this.count = count;
+        /**
+         * Количество циклов работы программы
+         */
+    private int value = 20;
+
+    /**
+     * Время блокировки потока
+     */
+    private int period;
+
+    /**
+     * Конструктор
+     */
+    public SimpleThread(int period) {
+        this.period = period;
     }
     @Override
     public void run() {
-        System.out.println("Поток " + Thread.currentThread().getName() + " начал работу:");
-        for(int i = 0; i < count; i++) {
-            System.out.println("- " + Thread.currentThread().getName());
+        System.out.println(String.format("Поток %s начал работу:",  Thread.currentThread().getName()));
+
+        for(int count = 1; count <= value; count++) {
+            System.out.println(String.format("- %s, (%s)", Thread.currentThread().getName(), count));
             try {
-                Thread.currentThread().sleep(1000);
+                Thread.currentThread().sleep(this.period);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
