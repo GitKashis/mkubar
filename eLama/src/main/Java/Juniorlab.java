09fac66007ace9ac8e646f8a39f9bc732b7fe61f
Mikhail Kubar <m.kubar@mail.ru>
2018-03-04 23:47:35 +0300

    eLama

--- eLama/src/main/Java/Juniorlab.java
+++ eLama/src/main/Java/Juniorlab.java
@@ -1,5 +1,41 @@
-import java.util.Arrays;
 
 public class Juniorlab {
 
+    //n - сумма цифр числа
+    //k - количество цифр в числе
+
+    void num (int n, int k) {
+        int count = 0;
+        int[] arr = new int[k];
+
+        for (int i = pow(10,k-1); i < pow(10,k); i++) {
+            String s = ""+i;
+            System.out.println(s);
+            for (int j = 0; j < k; j++) {
+                arr[j] = s.charAt(s.length() - j - 1);
+            }
+
+            if (sum(arr) == n) {
+                    count++;
+                    System.out.println(String.valueOf(i));
+            }
+        }
+        System.out.println("Found " + count + " numbers.");
+    }
+
+    int pow(int a, int b) {
+        int result = 1;
+        for (int i=1; i<=b; i++){
+            result = result*a;
+        }
+        return result;
+    }
+
+    int sum(int[] arr) {
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result += arr[i];
+        }
+        return result;
+    }
 }
---
+++ eLama/src/test/JuniorlabTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Time;
+
+import static org.junit.Assert.*;
+
+public class JuniorlabTest {
+
+
+    @Test
+    public void gcd_6() {
+        Juniorlab lab = new Juniorlab();
+        lab.num(10, 3);
+    }
+}
