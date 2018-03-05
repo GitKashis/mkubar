a118d36f93156fc441cba319f7dd761cdce703f4
Mikhail Kubar <m.kubar@mail.ru>
2018-03-05 18:55:53 +0300

    elama

--- eLama/src/main/Java/Juniorlab.java
+++ eLama/src/main/Java/Juniorlab.java
@@ -1,54 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Juniorlab {
 
-    //n - сумма цифр числа
-    //k - количество цифр в числе
+    public static int[] findAll(int n, int k){
+        // список для подходящих значений
+        List<Integer> list = new ArrayList<>();
+        // ищем в заданном диапазоне методом перебора значений
+        for (int i = (int) Math.pow(10,k-1); i < Math.pow(10,k); i++) {
+            //конвертируем для получения цифр поазрядно
+            String s = "" + i;
+            int arr[] = new int[s.length()];
 
-    void num (int n, int k) {
-        int count = 0;
-        int[] arr = new int[k];
-
-        for (int i = pow(10,k-1); i < pow(10,k); i++) {
-            String s = ""+i;
-
-            for (int j = 0; j < k; j++) {
-                arr[j] = (int)s.charAt(j);
-                System.out.println(arr[j]);
+            // переводим число в массив цифр
+            for (int count = 0; count < s.length(); count++) {
+                arr[count] = Integer.parseInt(String.valueOf(s.charAt(count)));
             }
-
-            if (sum(arr) == n) {
-                    count++;
-                    System.out.println(String.valueOf(i));
+            // отсеиваем лишнее и выбираем нужное
+            if (sum(arr) == n && !s.contains("0") && check(arr)) {
+                list.add(i);
             }
         }
-        System.out.println("Found " + count + " numbers.");
-    }
 
-    int pow(int a, int b) {
-        int result = 1;
-        for (int i=1; i<=b; i++){
-            result = result*a;
-        }
+        int[] result = list.isEmpty() ? new int[]{} : new int[]{list.size(), list.get(0), list.get(list.size()-1)} ;
         return result;
     }
-
-    int sum(int[] arr) {
-        int result = 0;
-        for (int i = 0; i < arr.length; i++) {
-            result += arr[i];
+    /**
+     * Метод подсчета суммы элементов массива.
+     */
+    private static int sum(int[] arr) {
+        return Arrays.stream(arr).sum();
+    }
+    /**
+     * Проверяем, идут ли цифры числа по возрастанию.
+     */
+    private static boolean check(int[] arr) {
+        boolean result = true;
+        for(int i = 0; i < arr.length - 1; i++) {
+            if(arr[i] > arr[i + 1]) {
+                result = false;
+                break;
+            }
         }
         return result;
-    }
-
-    public void test() {
-        int[] arr = new int[4];
-        String s = "1234";
-        for (int j = 0; j < 4; j++) {
-            arr[j] = s.charAt(j);
-            System.out.println((char)arr[j]);
-        }
-
-        int f = Integer.parseInt(String.valueOf((char)arr[0]));
-        System.out.println(f);
     }
 }
--- eLama/src/test/JuniorlabTest.java
+++ eLama/src/test/JuniorlabTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.Time;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +11,17 @@ public class JuniorlabTest {
 
     @Test
     public void gcd_6() {
-        Juniorlab lab = new Juniorlab();
-        //lab.num(10, 3);
-        lab.test();
+        int example01 = 35;
+        int example02 = 6;
+        int example11 = 10;
+        int example12 = 3;
+        int example21 = 27;
+        int example22 = 3;
+        int example31 = 84;
+        int example32 = 4;
+        assertEquals({123, 116999, 566666}, Juniorlab.findAll(example01, example02));
+        assertEquals({8, 118, 334}, Juniorlab.findAll(example11, example12));
+        assertEquals({1, 999, 999}, Juniorlab.findAll(example21, example22));
+        assertEquals({}, Juniorlab.findAll(example31, example32));
     }
 }
