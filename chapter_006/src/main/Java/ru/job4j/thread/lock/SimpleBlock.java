6a4f9ace0c40585fa488fa97d95c117b98b6b3d0
Mikhail Kubar <m.kubar@mail.ru>
2018-03-13 23:10:32 +0300

    SimpleLock добавлен Thread owner

--- chapter_006/src/main/Java/ru/job4j/thread/lock/SimpleBlock.java
+++ chapter_006/src/main/Java/ru/job4j/thread/lock/SimpleBlock.java
@@ -8,29 +8,32 @@ public class SimpleBlock {
     private boolean block = false;
 
     /**
+     * Текущий поток, одиаковый для метода lock и unlock.
+     */
+    private Thread owner;
+    /**
      * Method for block treads.
      * @throws InterruptedException if interrupt
      */
-    public void lock() {
-        synchronized (this) {
+    public synchronized void lock() {
             try {
                 while (block) {
                     wait();
                 }
                 block = true;
+                owner = Thread.currentThread();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-    }
 
     /**
      * Method for unlock treads.
      */
-    public void unlock() {
-        synchronized (this) {
-            block = false;
-            notifyAll();
-        }
+    public synchronized void unlock() {
+            if (Thread.currentThread() == owner) {
+                block = false;
+                notifyAll();
+            }
     }
 }
