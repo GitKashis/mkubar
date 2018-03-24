6a407b202544ce8c6350c2d1fb3c656b4094d62a
Mikhail Kubar <m.kubar@mail.ru>
2018-03-24 11:45:35 +0300

    eLama upside down

--- eLama/src/main/Java/Juniorlab.java
+++ eLama/src/main/Java/Juniorlab.java
@@ -2,32 +2,46 @@ import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Juniorlab {
 
     public static long upsidedown(String x, String y) {
         long count = 0;
-        for(BigInteger value = new BigInteger(x);
-            !value.equals(new BigInteger(y)); value = value.add(BigInteger.ONE)) {
-            if (symmetry(value.toString())) {
-                count++;
+        Pattern r = Pattern.compile("2|3|4|5|7");
+
+        for(BigInteger value = new BigInteger(x); !value.equals(new BigInteger(y)); value = value.add(BigInteger.ONE)) {
+            String str = value.toString();
+            Matcher m = r.matcher(str);
+            if (!m.find()) {
+                if(!Objects.equals(str, "6") || !Objects.equals(str, "9")) {
+                    if (symmetry(str)) count++;
+                }
+//                System.out.println(value);
             }
         }
         return count;
     }
 
     public static boolean symmetry(String str) {
-        final List<String> pair = Arrays.asList("00","11","88","96", "69");
         boolean result = true;
         String[] chars = str.split("(?!^)");
         for (int i = 0; i <= str.length() / 2; i++) {
-            String value = chars[i] + chars[str.length() - 1 - i];
-            if(!pair.contains(value)) {
+            String first = chars[i];
+            String last = chars[str.length() - 1 - i];
+            System.out.println(first + last);
+                if ((!Objects.equals(first, last))) {
+                    if ((Objects.equals(first + last, "69")) || (Objects.equals(first + last, "96"))) {
+                        continue;
+                    }
                 result = false;
                 break;
+                }
             }
-        }
         return result;
     }
+
 }
--- eLama/src/test/JuniorlabTest.java
+++ eLama/src/test/JuniorlabTest.java
@@ -31,16 +31,17 @@ public class JuniorlabTest {
         String example111 = "66478560783";
         String example112 = "174991074343062206";
 //        assertEquals(4282125l, Juniorlab.upsidedown(example01, example02));
-//        assertEquals(3l, Juniorlab.upsidedown(example11, example12));
-//        assertEquals(2l, Juniorlab.upsidedown(example21, example22));
-//        assertEquals(4l, Juniorlab.upsidedown(example31, example32));
-//        assertEquals(12l, Juniorlab.upsidedown(example41, example42));
-        assertEquals(718650l, Juniorlab.upsidedown(example51, example52));
+        assertEquals(3l, Juniorlab.upsidedown(example11, example12));
+        assertEquals(2l, Juniorlab.upsidedown(example21, example22));
+        assertEquals(4l, Juniorlab.upsidedown(example31, example32));
+        assertEquals(12l, Juniorlab.upsidedown(example41, example42));
+//        assertEquals(718650l, Juniorlab.upsidedown(example51, example52));
 //        assertEquals(78120000l, Juniorlab.upsidedown(example61, example62));
 //        assertEquals(74745418l, Juniorlab.upsidedown(example71, example72));
 //        assertEquals(2978125000l, Juniorlab.upsidedown(example81, example82));
 //        assertEquals(209808349609373l, Juniorlab.upsidedown(example91, example92));
 //        assertEquals(2775l, Juniorlab.upsidedown(example101, example102));
 //        assertEquals(1789100l, Juniorlab.upsidedown(example111, example112));
+        System.out.println(Juniorlab.symmetry("69"));
     }
 }
