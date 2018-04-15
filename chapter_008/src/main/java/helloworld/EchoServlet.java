65b716b190e25f3151a5d7b367946e94cc6da857
Mikhail Kubar <m.kubar@mail.ru>
2018-04-15 12:26:14 +0300

    EchoServlet starting

--- chapter_008/src/main/java/helloworld/EchoServlet.java
+++ chapter_008/src/main/java/helloworld/EchoServlet.java
@@ -1,6 +1,5 @@
-package echo;
+package helloworld;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,10 +8,10 @@ import java.io.PrintWriter;
 
 public class EchoServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/http");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.append("Hello world!");
+        writer.append("Hello World");
         writer.flush();
     }
 }
